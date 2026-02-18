cafcb3ac886848a6787f9b19dfffc30f2a6a184f
PLANB-John <planbeec@gmail.com>
2026-02-18 17:40:31 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import '../styles/globals.css'
-import Script from 'next/script' // [1] Next.js 전용 스크립트 컴포넌트 추가 [cite: 2026-02-17]
+import Script from 'next/script' // Next.js 전용 스크립트 컴포넌트 [cite: 2026-02-17]
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* [2] 구글 애드센스 엔진 가동 스크립트 [cite: 2026-02-17] */}
-      {/* 'ca-pub-XXXXXXXXXXXXXXXX' 부분은 나중에 승인 후 실제 본인의 ID로 바꾸시면 됩니다. */}
+      {/* [수정 완료] 대표님의 실제 구글 애드센스 ID를 적용했습니다 */}
       <Script
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-XXXXXXXXXXXXXXXX"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7078124525466670"
         crossOrigin="anonymous"
         strategy="afterInteractive" 
       />
