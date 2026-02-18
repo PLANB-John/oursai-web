bbb2f035b6cb48281b24145ea7869310cf72f7a9
PLANB-John <planbeec@gmail.com>
2026-02-18 17:54:58 +0900

    Update AdUnit.js

--- components/AdUnit.js
+++ components/AdUnit.js
@@ -14,8 +14,8 @@ export default function AdUnit() {
       <ins className="adsbygoogle"
            // [중요] height를 50px~60px로 고정하여 '이음' 사이트처럼 슬림하게 만듭니다.
            style={{ display: 'block', width: '100%', minHeight: '100px', maxHeight: '100px' }} 
-           data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" // 본인 번호 유지
-           data-ad-slot="XXXXXXXXXX"               // 본인 번호 유지
+           data-ad-client="ca-pub-7078124525466670" // 본인 번호 유지
+           data-ad-slot="2491735575"               // 본인 번호 유지
            // [중요] format을 horizontal로 고정하고, 자동 반응형 확장을 방지합니다.
            data-ad-format="horizontal"
            data-full-width-responsive="false"></ins> 
